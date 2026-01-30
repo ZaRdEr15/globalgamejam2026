7856d04f0e79f90fd57e052472801578a4a6508a
Urmas Rist <urmas@urist.ee>
2026-01-30 19:43:29 +0200

    feat: Add rectangle for player moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include "raylib.h"
 
+
+constexpr float PLAYER_SPEED = 5;
+
+void moveRectPlayer(Rectangle &rect) {
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+        rect.x -= PLAYER_SPEED;
+    } else if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+        rect.x += PLAYER_SPEED;
+    }
+}
+
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
+
+    Rectangle rect = {
+        .x = screenWidth / 2,
+        .y = screenHeight / 2,
+        .width = screenWidth / 10,
+        .height = screenHeight / 10
+    };
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);
 
-    // Detect window close button or ESC key 
+    // Detect window close button or ESC key
     while (!WindowShouldClose()) {
+        moveRectPlayer(rect);
         BeginDrawing();
-
             ClearBackground(RAYWHITE);
-
+            DrawRectangleRec(rect, RED);
             DrawText("Hello, world!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
